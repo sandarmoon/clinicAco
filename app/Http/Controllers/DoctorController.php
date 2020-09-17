11ab50a484b465a88ca3c5f710966168c84aeef7
ayechanoo <sandarmoon2361996@gmail.com>
2020-09-17 15:15:18 +0630

    adding admin dashboard

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -44,9 +44,7 @@ class DoctorController extends Controller
         ->orderBy('A_Date', 'ASC');
 
 
-        },'referredFrom.patient.treatments'=>function($e){
-            $e->whereNotNull('gc_level');
-        }])
+        },'referredFrom.patient'])
         ->withCount(['treatments'=>function($q1){
             $q1->whereNotNull('gc_level');
         },'appointments'=> function($q) {
@@ -66,7 +64,7 @@ class DoctorController extends Controller
         // $survey=Appointment::where('doctor_id',$doctor_id->id)
         // ->where('status',0)
         // ->get();
-          //dd($survey);
+         // dd($survey);
           // dd($wpatients);
 
         return view('doctor.doctordashboard',compact('survey','wpatients'));

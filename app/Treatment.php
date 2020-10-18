1af98567f03b075c221afbd16db53c6bae2a9677
ayechanoo <sandarmoon2361996@gmail.com>
2020-10-18 11:14:58 +0630

    fixing

--- app/Treatment.php
+++ app/Treatment.php
@@ -9,7 +9,7 @@ use DB;
 
 class Treatment extends Model
 {
-	use SoftDeletes,EagerLoadPivotTrait;
+	use SoftDeletes;
      protected $fillable = ['file','gc_level','temperature','body_weight','spo2','pr','bp','rbs','complaint','examination','relevant_info','chronic_disease','diagnosis','external_medicine','next_visit_date','next_visit_date2','patient_id','doctor_id','charges'];
 
 

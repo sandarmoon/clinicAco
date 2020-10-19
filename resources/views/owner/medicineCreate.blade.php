d899e4a0313d06b316983412a2e17be61d80433a
ayechanoo <sandarmoon2361996@gmail.com>
2020-10-19 16:33:48 +0630

    h

--- resources/views/owner/medicineCreate.blade.php
+++ resources/views/owner/medicineCreate.blade.php
@@ -1014,7 +1014,7 @@
                       { "data": "chemical"
                       } ,
                       { "data": "phar"
-                      }
+                      
                       } ,
                       { "data": "bu"
                       } ,

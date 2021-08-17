64fde5eb26bd025e437baa62ee074ccdc408d5bd
ayechanoo <sandarmoon2361996@gmail.com>
2021-08-17 16:52:08 +0630

    adding and fixing

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,7 +41,7 @@ class LoginController extends Controller
     }
      protected function authenticated(Request $request, $user)
     {
-        
+        // dd($user->getRoleNames());
          // dd($request);
         if($user->hasRole('Reception')){
             return redirect('/rdashboard');
